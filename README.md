94cebc32c84b8b1373a03704fa03071df686e794
Mikael Lindahl <mickelindahl@gmail.com>
2022-07-10 18:08:40 +0200

    seems done

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # loopia-dns
-Update loopia dns with ip that changes
+Update loopia dns when your ip that changes
+
+## Install
+
+Run `cp sample.env .env` and edit credentials in`.env`
+
+Then tun `./install.sh`
+
+Done!
+
+
--- sample.loopia-dns.sh
+++ sample.loopia-dns.sh
@@ -4,12 +4,10 @@
 MAILTO={mailto}
 
 # First the adress we want to update, CHANGE THIS to your address
-hostname={hostname}
+HOSTNAME={hostname}
+PASSWORD={password}
 
 # Second the login:pass we'll use, CHANGE THIS to your login and password
-cred='$hostname:lpfmnxps'
-
-echo "Cred: $cred"
 
 # Get IP adress from Loopia
 myip=`curl -s dns.loopia.se/checkip/checkip.php | sed 's/^.*: \([^<]*\).*$/\1/'`
@@ -33,38 +31,13 @@ then
   exit 255
 fi
 
-# Check if we already set this ip-adress
-# We should really check DNS but since there is no good
-# general command line that works similar on most UNIX
-# we can't rely on it.
-
-#tempdir="$TMP"
-#[ x"$tempdir" = x ] && tempdir="$TMPDIR"
-#[ x"$tempdir" = x ] && tempdir=/tmp
-
-#ipfile="$tempdir"/"$hostname".ip
-#echo $ipfile
-#if [ -r "$ipfile" ]
-#then
-  #fmyip=`grep '^'"$myip"'$' "$ipfile"`
-
-# Fix to check what ip is currently active 
-fmyip="$(cut -d' ' -f4 <<<`host $hostname`)"
+# Fix to check what ip is currently active
+fmyip="$(cut -d' ' -f4 <<<`host $HOSTNAME`)"
 if [ x"$myip" = x"$fmyip" ]
 then
   echo No change detected. Wont run update.
   exit 0
 fi
-
-#fi
-
-# Store the IP for future updates
-#if [ -d "$tempdir" -a -w "$tempdir" ]
-#then
-#  echo "$myip" > "$ipfile"
-#else
-#  echo Warning, tempdir do not exist or is not writable: "$tempdir"
-#fi
 
 # Base URL
 burl=https://dns.loopia.se/XDynDNSServer/XDynDNS.php
@@ -72,26 +45,30 @@ burl=https://dns.loopia.se/XDynDNSServer/XDynDNS.php
 # Extra options
 eops='wildcard=NOCHG'
 
+# Credentials
+cred="$HOSTNAME:$PASSWORD"
+
 # Build URL
-url="$burl"'?hostname='"$hostname"'&'myip="$myip"'&'"$eops"
+url="$burl"'?hostname='"$HOSTNAME"'&'myip="$myip"'&'"$eops"
 echo $url
 response=`curl -s --user "$cred" "$url"`
 
 if [ x"$response" = x"good" ]; then
 
   MSG="Loopia dns update for debianserver - good $fmyip -> $myip"
+  echo "$MSG"
   mail -s "Success loopia dns update" $MAILTO <<< "$MSG"
 
 elif [ x"$response" = x"nochg" ]; then
 
-  MSG="Loopia dns update for debianserver - no change. Ip for $hostname still is $myip"
+  MSG="Loopia dns update for debianserver - no change. Ip for $HOSTNAME still is $myip"
+  echo "$MSG"
   mail -s "Skipping loopia dns update" $MAILTO <<< "$MSG"
 
 else
 
   MSG="Loopia dns update for debianserver - failed. Please check loopiadns.sh script. Error message: $response"
+  echo "$MSG"
   mail -s "Failed loopia dns update" $MAILTO <<< "$MSG"
 
 fi
-
-echo
